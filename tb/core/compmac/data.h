c06fee3784b2f5bb48e9c4e1f3c9dc8b933b8352
Joan Mihali <jmihali@student.ethz.ch>
2022-05-09 09:33:14 +0200

    write simple test program using compressedMAC

---
+++ tb/core/compmac/data.h
@@ -0,0 +1,9 @@
+#ifndef DATA_H
+#define DATA_H
+
+#define N_STIMULI 3
+
+uint32_t data1[N_STIMULI] = {1, 2, 3};
+uint32_t data2[N_STIMULI] = {10, 20, 30};
+
+#endif DATA_H
--- tb/core/compmac/test.c
+++ tb/core/compmac/test.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "data.h"
 
 int main(int argc, char *argv[])
 {
-    /* inline assembly */
-    asm volatile("ecall");
-    /* write something to stdout */
-    printf("hello world!\n");
+    uint32_t op1, op2, res;
+
+    for (int i=0; i<N_STIMULI; i++){
+      asm volatile(
+        "lw %[op1], 0x(%[data1_addr]);"
+        "lw %[op2], 0x(%[data2_addr]);"
+        "pv.mlsdotusp.t.0 %[res], %[op1], %[op2];"
+                : [op1] "+r" (op1), [op2] "+r" (op2), [res] "=r" (res)
+                : [data1_addr] "r" (&data1[0]), [data2_addr] "r" (&data2[0]));
+      printf("x=0x%x (@0x%x)\ty=0x%x (@0x%x)\t res=0x%x\n", data1[i], &data1[i], data2[i], &data2[i], res);
+    }
+
     return EXIT_SUCCESS;
 }
