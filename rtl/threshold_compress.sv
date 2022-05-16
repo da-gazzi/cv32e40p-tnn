d05b8c518b8041d688dcc9a06f161e3df963ead7
Joan Mihali <jmihali@student.ethz.ch>
2022-05-16 13:38:50 +0200

    remove internal registers in threshold_compress and delegate their functionality to external registers

--- rtl/threshold_compress.sv
+++ rtl/threshold_compress.sv
@@ -7,21 +7,24 @@ module threshold_compress
   parameter COUNTER_WIDTH = $clog2(COUNTER_MAX)
 )
 (
-  input  logic signed [31:0]              data_i,
+  input  logic signed [31:0]              preactivation_i,
   input  logic        [31:0]              thresholds_i,
+  input  logic        [COUNTER_WIDTH-1:0] counter_i,        // previous counter state
+  input  logic        [COMPREG_WIDTH-1:0] precompressed_i,  // previously computed activations
+  input  logic        [OUTPUT_WIDTH-1:0]  compressed_i,     // previously compressed activations
+
   input  logic                            enable_i,
   input  logic                            rst_ni,
   input  logic                            clk_i,
-  output logic        [OUTPUT_WIDTH-1:0]  data_o,
-  output logic        [COUNTER_WIDTH-1:0] counter_o,
-  output logic                            compreg_full_o  // signalizes the compression register is full
+
+  output logic       [COUNTER_WIDTH-1:0]  counter_o,        // counter state
+  output logic       [COMPREG_WIDTH-1:0]  precompressed_o,  // computed activations
+  output logic       [OUTPUT_WIDTH-1:0]   compressed_o,
+  output logic                            compreg_full_o    // signalizes the compression register is full
 );
 
   logic signed [15:0]              threshold_lo, threshold_hi;
   logic signed [1:0]               activation;
-
-  logic [COUNTER_WIDTH:0]          cnt_d, cnt_q;
-  logic [COMPREG_WIDTH/2-1:0][1:0] compreg_d, compreg_q;
 
   logic [COMPREG_WIDTH/2-1:0][1:0] encoder_in;
   logic [OUTPUT_WIDTH-1:0]         encoder_out;
@@ -30,35 +33,32 @@ module threshold_compress
     activation = '0;
     encoder_in = '0;
 
-    compreg_d = '0;
-    cnt_d = cnt_q;
-
-    compreg_full_o = 1'b0;
-    data_o = '0;
     counter_o = '0;
+    precompressed_o = '0;
+    compreg_full_o = 1'b0;
+    compressed_o = '0;
 
     threshold_hi = thresholds_i[15:0];
     threshold_lo = thresholds_i[31:16];
 
     if (enable_i) begin
-      if (data_i < threshold_lo) begin
+      if (preactivation_i < threshold_lo) begin
         activation = -2'd1;
-      end else if (data_i >= threshold_hi) begin
+      end else if (preactivation_i >= threshold_hi) begin
         activation = 2'd1;
       end
 
-      encoder_in = (activation << (cnt_q << 1)) | compreg_q;
+      encoder_in = (activation << COMPREG_WIDTH'(counter_i << 1)) | precompressed_i;
 
-      if (cnt_q < COUNTER_MAX-1) begin
-        compreg_d = encoder_in;
-        cnt_d = cnt_q + 1;
-      end else if (cnt_q == COUNTER_MAX-1) begin
-        cnt_d = '0;
+      if (counter_i < COUNTER_MAX-1) begin
+        precompressed_o = encoder_in;
+        counter_o = counter_i + 1;
+      end else if (counter_i == COUNTER_MAX-1) begin
+        counter_o = '0;
         compreg_full_o = 1'b1;
       end
 
-      data_o = encoder_out;
-      counter_o = cnt_q;
+      compressed_o = encoder_out;
     end
   end
 
@@ -71,15 +71,5 @@ module threshold_compress
       );
     end
   endgenerate
-
-  always_ff @(posedge clk_i, negedge rst_ni) begin
-    if (!rst_ni) begin
-      cnt_q     <= '0;
-      compreg_q <= '0;
-    end else if (enable_i) begin
-      cnt_q     <= cnt_d;
-      compreg_q <= compreg_d;
-    end
-  end
 
 endmodule
