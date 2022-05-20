113423b2a82572aebb2129f9bd5d1b528955099e
Joan Mihali <jmihali@student.ethz.ch>
2022-05-20 15:26:11 +0200

    restructure synopsys lib

--- gf22fdx/synopsys/scripts/ungroup_script.tcl
+++
@@ -1,3 +0,0 @@
-ungroup [get_cells l2_ram_i/*]                        -flatten
-ungroup [get_cells fc_subsystem_i/FC_CORE.lFC_CORE/*] -flatten
-ungroup [get_cells i_soc_interconnect_wrap/*]         -flatten
--- gf22fdx/synopsys/start_synth.csh
+++
@@ -1,3 +0,0 @@
-#!/bin/tcsh
-setenv SITE ZH
-synopsys-2022.03 dc_shell-xg-t -64 -f ./scripts/go_synth.tcl
