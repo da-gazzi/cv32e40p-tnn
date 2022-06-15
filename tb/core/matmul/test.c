a6d385ceb49ffc13ccdb20b86e10386aa2f23bb3
Joan Mihali <jmihali@student.ethz.ch>
2022-06-15 16:26:30 +0200

    write matmul kernel as separate function

--- tb/core/matmul/test.c
+++ tb/core/matmul/test.c
@@ -6,84 +6,40 @@
 
 #define PACK_INT2_SIZE(x)                                    ((x) >> 2)
 
-// does not work somehow
 #define CompressedMAC(sum, ptr, config) asm volatile(                \
     "pv.smlsdotp.t %[shum], %[phtr], %[chonfig];"                    \
     : [shum] "+r" (sum), [phtr] "+r" (ptr): [chonfig] "I" (config))
 
-//struct MyStruct {
-//  int32_t sum;
-//  uint32_t ptr;
-//};
-
-//inline struct MyStruct CompressedMAC(int32_t sum, uint32_t ptr, char config) {
-//  struct MyStruct x;
-//  asm volatile(
-//    "pv.smlsdotp.t %[sum], %[ptr], %[config];"
-//    : [sum] "+r" (sum), [ptr] "+r" (ptr) : [config] "I" (config)
-//  );
-//  x.sum = sum;
-//  x.ptr = ptr;
-//  return x;
-//};
-
 #define InitNNRF(ptr, config) asm volatile(        \
     "pv.smlsdotp.t x0, %[phtr], %[chonfig];"       \
     : [phtr] "+r" (ptr) : [chonfig] "I" (config))
-
-//inline uint32_t InitNNRF(uint32_t ptr, char config) {
-//  asm volatile(
-//    "pv.smlsdotp.t x0, %[ptr], %[config];"
-//    : [ptr] "+r" (ptr) : [config] "I" (config)
-//  );
-//  return ptr;
-//};
 
 #define ThresholdCompress(res, val, thrs) asm volatile(                                                \
     "pv.thrc %[rhes], %[vhal], %[thhrs];" : [rhes] "+r" (res) : [vhal] "r" (val), [thhrs] "r" (thrs))
 
 #define GetConfig(a_update, b_update, a_reg, b_reg) a_update << 4 | b_update << 3 | a_reg << 1 | b_reg
 
-//static inline uint32_t ThresholdCompress(uint32_t res, int32_t val, uint32_t thrs) {
-//  asm volatile(
-//    "pv.thrc %[res], %[val], %[thrs];" : [res] "+r" (res) : [val] "r" (val), [thrs] "r" (thrs)
-//  );
-//  return res;
-//};
-
-//static inline uint8_t * check_store(uint32_t res, uint8_t *pOut) {
-//  //printf("in check_store: res=%x\n", res);
-//  // if counter value equals 0 (i.e. 5 computations are finished), store the compressed output to the output pointer
-//  if ((res & 0xe0000000) == 0x00000000) {
-//    *pOut = res & 0xff;
-//    pOut++;
-//  }
-//  return pOut;
-//};
-
 #define check_store(res, pOut)            \
   if ((res & 0xe0000000) == 0x00000000) { \
     *pOut = res & 0xff;                   \
     pOut++; }
 
-
-int main(int argc, char *argv[])
+uint8_t * __attribute__((noinline)) xpulp_nn_matmul_ternary(
+                        int8_t   *pIn,
+                        int8_t   *pBias,
+                        uint32_t *pThr,
+                        int8_t   *pOut,
+                        int8_t   *pOut2,
+                        int8_t   *pWeight,
+                        uint16_t num_col_im2col,
+                        uint16_t ch_out)
 {
-  int ch_out;
-  ch_out = 4;
-  int8_t *pBias = NULL;
-  uint32_t num_col_im2col = NUM_COL_IM2COL; // the weights and acts are compressed
-
   uint16_t ch_out_r = PACK_INT2_SIZE(ch_out);
 
   uint16_t num_col_im2col_w = PACK_INT2_SIZE(num_col_im2col); // in how many bytes do the activations fit?
   uint16_t num_col_im2col_a = PACK_INT2_SIZE(num_col_im2col);
-  uint8_t outputs[20] = {0};
 
   int n_outputs = ceil((ch_out >> 2)*1.6); // should be floor instead?
-
-  uint8_t *pOut1 = &outputs[0];
-  uint8_t *pOut2 = &outputs[n_outputs/2];
 
   uint8_t *pA = pWeight;
 
@@ -103,8 +59,6 @@ int main(int argc, char *argv[])
     uint32_t *ptrA2 = (uint32_t *) pA2;
     uint32_t *ptrA3 = (uint32_t *) pA3;
     uint32_t *ptrA4 = (uint32_t *) pA4;
-
-    uint32_t valA, valA2, valA3, valA4, valB, valB2;
 
     InitNNRF(ptrA,  GetConfig(1, 0, 0, 0));
     InitNNRF(ptrA2, GetConfig(1, 0, 1, 0));
@@ -165,6 +119,7 @@ int main(int argc, char *argv[])
       pB+=loop_cnt_im2col_a;
       pB2+=loop_cnt_im2col_a;
 
+      volatile uint32_t valA, valA2, valA3, valA4, valB, valB2; // volatile somehow solves the problem with XX
       // pack the remaining weights and activations into 32-bit vectors
       // padding with 0xd9 because ternary_decoder(0xd9) = 0000000000
       if (col_cnt_im2col == 4)
@@ -235,16 +190,16 @@ int main(int argc, char *argv[])
     //printf("sum8 = %d\n", sum8);
 
     ThresholdCompress(res1, sum, pThr[0]);
-    check_store(res1, pOut1);
+    check_store(res1, pOut);
 
     ThresholdCompress(res1, sum2, pThr[1]);
-    check_store(res1, pOut1);
+    check_store(res1, pOut);
 
     ThresholdCompress(res1, sum3, pThr[2]);
-    check_store(res1, pOut1);
+    check_store(res1, pOut);
 
     ThresholdCompress(res1, sum4, pThr[3]);
-    check_store(res1, pOut1);
+    check_store(res1, pOut);
 
     ThresholdCompress(res2, sum5, pThr[0]);
     check_store(res2, pOut2);
@@ -258,6 +213,33 @@ int main(int argc, char *argv[])
     ThresholdCompress(res2, sum8, pThr[3]);
     check_store(res2, pOut2);
   }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int ch_out;
+  ch_out = 4;
+  int8_t *pBias = NULL;
+  uint32_t num_col_im2col = NUM_COL_IM2COL; // the weights and acts are compressed
+
+  uint8_t outputs[20] = {0};
+
+  int n_outputs = ceil((ch_out >> 2)*1.6); // should be floor instead?
+
+  uint8_t *pOut1 = &outputs[0];
+  uint8_t *pOut2 = &outputs[n_outputs/2];
+
+  pOut1 = xpulp_nn_matmul_ternary(
+    pIn,
+    pBias,
+    pThr,
+    pOut1,
+    pOut2,
+    pWeight,
+    num_col_im2col,
+    ch_out
+  );
 
   int n_mismatches = 0;
   for(int i=0; i < n_outputs; i++) {
